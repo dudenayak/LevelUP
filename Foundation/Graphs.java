fc30c11ba0576ffd1259e862de9a21455b7acb80
dudenayak <deveshjain2k1@gmail.com>
2021-08-04 00:14:35 +0530

    Update Graphs.java

--- Foundation/Graphs.java
+++ Foundation/Graphs.java
@@ -5,23 +5,23 @@ import java.util.*;
 
 public class Graphs {
 
-    //hasPath
+    // hasPath
     boolean[] visited = new boolean[vtces];
     boolean path = hasPath(graph, src, dest, visited);
 
     System.out.println(path);
 
-    public static boolean hasPath(ArrayList<Edge>[] graph, int src, int dest, boolean[] visited ){
+    public static boolean hasPath(ArrayList<Edge>[] graph, int src, int dest, boolean[] visited) {
 
-        if(src==dest){
+        if (src == dest) {
             return true;
         }
 
         visited[src] = true;
-        for(Edge edge : graph[src] ){
-            if(visited[edge.nbr] == false){
+        for (Edge edge : graph[src]) {
+            if (visited[edge.nbr] == false) {
                 boolean hasNbrPath = hasPath(graph, edge.nbr, dest, visited);
-                if(hasNbrPath == true){
+                if (hasNbrPath == true) {
                     return true;
                 }
             }
@@ -29,6 +29,67 @@ public class Graphs {
         return false;
     }
 
-    //Print all paths
+    // Print all paths
+
+    boolean[] visited = new boolean[vtces];
+
+    printAllPaths(graph, src, dest, visited, src + "");
+
+    public static void printAllPaths(ArrayList<Edge>[] graph, int src, int dest, boolean[] visited, String psf) {
+
+        if (src == dest) {
+            System.out.println(psf);
+            return;
+        }
+
+        visited[src] = true;
+        for (Edge edge : graph[src]) {
+            if (visited[edge.nbr] == false) {
+                printAllPaths(graph, edge.nbr, dest, visited, psf + edge.nbr);
+            }
+        }
+        visited[src] = false;
+    }
+
+    // Multisolver - Smallest, Longest, Ceil, Floor, Kthlargest Path
+
+    public static void multisolver(ArrayList<Edge>[] graph, int src, int dest, boolean[] visited, int criteria, int k,
+            String psf, int wsf) {
+        if (src == dest) {
+            if (wsf < spathwt) {
+                spathwt = wsf;
+                spath = psf;
+            }
+            if (wsf > lpathwt) {
+                lpathwt = wsf;
+                lpath = psf;
+            }
+            if (wsf > criteria && wsf < cpathwt) {
+                cpathwt = wsf;
+                cpath = psf;
+            }
+            if (wsf < criteria && wsf > fpathwt) {
+                fpathwt = wsf;
+                fpath = psf;
+            }
+            if (pq.size() < k) {
+                pq.add(new Pair(wsf, psf));
+            } else {
+                if (wsf > pq.peek().wsf) {
+                    pq.remove();
+                    pq.add(new Pair(wsf, psf));
+                }
+            }
+            return;
+        }
+        visited[src] = true;
+        for (Edge e : graph[src]) {
+            if (visited[e.nbr] == false) {
+                multisolver(graph, e.nbr, dest, visited, criteria, k, psf + e.nbr, wsf + e.wt);
+            }
+        }
+        visited[src] = false;
+
+    }
 
 }
