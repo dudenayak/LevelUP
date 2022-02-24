19a62d35025a11add98c19db0169c2d82fe55bf0
dudenayak <deveshjain2k1@gmail.com>
2022-02-24 14:32:43 +0530

    Update Class6.java

--- Level2/Recursion/Class6.java
+++ Level2/Recursion/Class6.java
@@ -1,5 +1,17 @@
 package Level2.Recursion;
 
 public class Class6 {
+    public static void leftShift() {
+        int n = 5;
+        for (int i = 1; i <= 4; i++) {
+            System.out.println(n << i);
+        }
+    }
 
+    public static void rightShift() {
+        int n = 80;
+        for (int i = 1; i <= 4; i++) {
+            System.out.println(n >> i);
+        }
+    }
 }
