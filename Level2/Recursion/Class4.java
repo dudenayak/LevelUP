fde31f2346e675601d521b095392dca8dedae2f8
dudenayak <deveshjain2k1@gmail.com>
2022-02-24 10:33:22 +0530

    class5

--- Level2/Recursion/Class4.java
+++ Level2/Recursion/Class4.java
@@ -164,7 +164,7 @@ public class Class4 {
         return count;
     }
 
-    int queenPermutation(Boolean[] boxes, int tnq, int bno, int qno, String ans) {
+    public static int queenPermutation(Boolean[] boxes, int tnq, int bno, int qno, String ans) {
         if (qno == tnq) {
             System.out.println(ans);
             return 1;
@@ -182,7 +182,7 @@ public class Class4 {
         return count;
     }
 
-    int queenCombination2D(Boolean[][] board, int tnq, int bno, String ans) {
+    public static int queenCombination2D(Boolean[][] board, int tnq, int bno, String ans) {
         if (tnq == 0) {
             System.out.println(ans);
             return 1;
@@ -198,7 +198,7 @@ public class Class4 {
         return count;
     }
 
-    int queenPermutation2D(Boolean[][] board, int tnq, int bno, String ans) {
+    public static int queenPermutation2D(Boolean[][] board, int tnq, int bno, String ans) {
         if (tnq == 0) {
             System.out.println(ans);
             return 1;
@@ -216,6 +216,185 @@ public class Class4 {
         }
 
         return count;
+    }
+
+    public static boolean isSafeToPlaceQueen_C(Boolean[][] board, int row, int col) {
+
+        int[][] dir = { { 0, -1 }, { -1, -1 }, { -1, 0 }, { -1, 1 } };
+
+        int n = board.length, m = board[0].length;
+        for (int d = 0; d < n; d++) {
+            for (int rad = 1; rad < board.length; rad++) {
+                int r = row + rad * dir[d][0];
+                int c = col + rad * dir[d][1];
+
+                if (r >= 0 && c >= 0 && r < n && c < m) {
+                    if (board[r][c])
+                        return false;
+                } else
+                    break;
+            }
+        }
+        return true;
+    }
+
+    public static int nqueen_01_combination(Boolean[][] board, int tnq, int idx, String ans) {
+        if (tnq == 0) {
+            System.out.println(ans);
+            return 1;
+        }
+
+        int count = 0, n = board.length, m = board[0].length;
+        for (int i = idx; i < n * m; i++) {
+            int r = i / m;
+            int c = i % m;
+            if (isSafeToPlaceQueen_C(board, r, c)) {
+                board[r][c] = true;
+                count += nqueen_01_combination(board, tnq - 1, i + 1, ans + "(" + r + "," + c + ") ");
+                board[r][c] = false;
+            }
+        }
+
+        return count;
+
+    }
+
+    public static int nqueen_01_combination_sub(Boolean[][] board, int tnq, int idx, String ans) {
+
+        int count = 0, n = board.length, m = board[0].length;
+        if (tnq == 0 || idx == n * m) {
+            if (tnq == 0) {
+                System.out.println(ans);
+            }
+            return tnq == 0 ? 1 : 0;
+        }
+        int r = idx / m;
+        int c = idx % m;
+        if (isSafeToPlaceQueen_C(board, r, c)) {
+            board[r][c] = true;
+            count += nqueen_01_combination_sub(board, tnq - 1, idx + 1, ans + "(" + r + "," + c + ") ");
+            board[r][c] = false;
+        }
+        count += nqueen_01_combination_sub(board, tnq, idx + 1, ans);
+
+        return count;
+
+    }
+
+    static boolean[] row;
+    static boolean[] col;
+    static boolean[] diag;
+    static boolean[] aDiag;
+
+    int calls = 0;
+
+    public static int nqueen_02_combi(int n, int m, int tnq, int idx, String ans) {
+        if (tnq == 0) {
+            System.out.println(ans);
+            return 1;
+        }
+
+        int count = 0;
+        // calls++;
+        for (int i = idx; i < n * m; i++) {
+            int r = i / m;
+            int c = i % m;
+            if (!row[r] && !col[c] && !diag[r + c] && !aDiag[r - c + m - 1]) {
+                row[r] = col[c] = diag[r + c] = aDiag[r - c + m - 1] = true;
+                count += nqueen_02_combi(n, m, tnq - 1, i + 1, ans + "(" + r + "," + c + ") ");
+                row[r] = col[c] = diag[r + c] = aDiag[r - c + m - 1] = false;
+            }
+        }
+
+        return count;
+    }
+
+    public static int nqueen_03_combi(int n, int m, int tnq, int floor, String ans) {
+        if (tnq == 0) {
+            System.out.println(ans);
+            return 1;
+        }
+
+        int count = 0;
+        // calls++;
+        for (int room = 0; room < m; room++) {
+            int r = floor, c = room;
+            if (!row[r] && !col[c] && !diag[r + c] && !aDiag[r - c + m - 1]) {
+                row[r] = col[c] = diag[r + c] = aDiag[r - c + m - 1] = true;
+                count += nqueen_03_combi(n, m, tnq - 1, floor + 1, ans + "(" + r + "," + c + ") ");
+                row[r] = col[c] = diag[r + c] = aDiag[r - c + m - 1] = false;
+            }
+        }
+
+        return count;
+    }
+
+    static int cols = 0;
+    static int diags = 0;
+    static int aDiags = 0;
+
+    public static int nqueen_04_combi(int n, int floor) {
+        if (floor == n) {
+            return 1;
+        }
+
+        int count = 0, m = n;
+        for (int room = 0; room < n; room++) {
+            int r = floor, c = room;
+            if ((cols & (1 << c)) == 0 && (diags & (1 << (r + c))) == 0 && (aDiags & (1 << (r - c + m - 1))) == 0) {
+                cols ^= (1 << c);
+                diags ^= (1 << (r + c));
+                aDiags ^= (1 << (r - c + m - 1));
+
+                count += nqueen_04_combi(n, floor + 1);
+
+                cols ^= (1 << c);
+                diags ^= (1 << (r + c));
+                aDiags ^= (1 << (r - c + m - 1));
+            }
+        }
+
+        return count;
+    }
+
+    public static boolean isSafeToPlaceQueen_P(Boolean[][] board, int row, int col) {
+
+        int[][] dir = { { 0, -1 }, { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, 1 }, { 1, 1 }, { 1, 0 }, { 1, -1 } };
+        int n = board.length, m = board[0].length;
+        for (int d = 0; d < dir.length; d++) {
+            for (int rad = 1; rad < board.length; rad++) {
+                int r = row + rad * dir[d][0];
+                int c = col + rad * dir[d][1];
+
+                if (r >= 0 && c >= 0 && r < n && c < m) {
+                    if (board[r][c])
+                        return false;
+                } else
+                    break;
+            }
+        }
+        return true;
+    }
+
+    public static int nqueen_01_permutation(Boolean[][] board, int tnq, int idx, String ans) {
+        if (tnq == 0) {
+            System.out.println(ans);
+            return 1;
+        }
+
+        int count = 0, n = board.length, m = board[0].length;
+        for (int i = idx; i < n * m; i++) {
+            int r = i / m;
+            int c = i % m;
+            if (isSafeToPlaceQueen_P(board, r, c)) {
+                board[r][c] = true;
+                count += nqueen_01_permutation(board, tnq - 1, 0, ans + "(" + r + "," + c + ") ");
+                board[r][c] = false;
+            }
+        }
+
+        return count;
+
     }
 
 }
--- Level2/Recursion/Class5.java
+++ Level2/Recursion/Class5.java
@@ -1,5 +1,163 @@
 package Level2.Recursion;
 
-public class Class5 {
+import java.util.*;
 
+public class Class5 {
+    class Solution {
+        boolean[] row;
+        boolean[] col;
+        boolean[] diag;
+        boolean[] aDiag;
+
+        public int nqueen_03_combi(int n, int floor) {
+            if (floor == 0) {
+                return 1;
+            }
+
+            int count = 0;
+            for (int room = 0; room < n; room++) {
+                int r = floor, c = room;
+                if (!row[r] && !col[c] && !diag[r + c] && !aDiag[r - c + n - 1]) {
+                    row[r] = col[c] = diag[r + c] = aDiag[r - c + n - 1] = true;
+                    count += nqueen_03_combi(n, floor + 1);
+                    row[r] = col[c] = diag[r + c] = aDiag[r - c + n - 1] = false;
+                }
+            }
+
+            return count;
+        }
+
+        public int totalNQueens(int n) {
+            row = new boolean[n];
+            col = new boolean[n];
+            diag = new boolean[2 * n - 1];
+            aDiag = new boolean[2 * n - 1];
+            return nqueen_03_combi(n, 0);
+        }
+    }
+
+    // combination sum ii
+    private void findCombinations(int index, int[] array, int target, Set<List<Integer>> ans, List<Integer> ds) {
+
+        if (index == array.length) {
+            if (target == 0) {
+                ans.add(new ArrayList<>(ds));
+
+            }
+            return;
+        }
+
+        if (array[index] <= target) {
+            ds.add(array[index]);
+
+            findCombinations(index + 1, array, target - array[index], ans, ds);
+            ds.remove(ds.size() - 1);
+        }
+        findCombinations(index + 1, array, target, ans, ds);
+    }
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+
+        Set<List<Integer>> ans = new HashSet<>();
+        Arrays.sort(candidates);
+        findCombinations(0, candidates, target, ans, new ArrayList<>());
+
+        List<List<Integer>> op = new ArrayList<>();
+        for (List a : ans) {
+            op.add(a);
+        }
+        return op;
+    }
+
+    // coin change
+    public int coinChange(int[] coins, int val) {
+        int[] dp = new int[val + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    int subres = dp[i - coins[j]];
+
+                    if (subres != Integer.MAX_VALUE) {
+                        dp[i] = Math.min(subres + 1, dp[i]);
+                    }
+                }
+            }
+        }
+
+        if (dp[val] == Integer.MAX_VALUE) {
+            return -1;
+        }
+        return dp[val];
+    }
+
+    // n queens
+    public List<List<String>> solveNQueens(int n) {
+
+        boolean[][] board = new boolean[n][n];
+
+        List<List<String>> list = new ArrayList<>(queen("", board, 0));
+        return list;
+
+    }
+
+    public static List<List<String>> queen(String p, boolean[][] board, int row) {
+        if (row == board.length) {
+            List<List<String>> list = new ArrayList<>();
+            list.add(addQ(board));
+            return list;
+        }
+
+        List<List<String>> list = new ArrayList<>();
+
+        for (int col = 0; col < board.length; col++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = true;
+                list.addAll(queen(p, board, row + 1));
+                board[row][col] = false;
+            }
+        }
+        return list;
+    }
+
+    private static List<String> addQ(boolean[][] board) {
+        List<String> list = new ArrayList<>();
+
+        for (boolean[] arr : board) {
+            StringBuilder s = new StringBuilder();
+            for (boolean element : arr) {
+                if (element)
+                    s = s.append('Q');
+                else
+                    s = s.append('.');
+            }
+            list.add(s.toString());
+        }
+        return list;
+    }
+
+    private static boolean isSafe(boolean[][] board, int row, int col) {
+        // vertical checkUP
+        for (int i = 0; i < row; i++) {
+            if (board[i][col])
+                return false;
+        }
+        // check Diagonal left
+        int maxLeft = Math.min(row, col);
+        for (int i = 0; i <= maxLeft; i++) {
+            if (board[row - i][col - i])
+                return false;
+        }
+        // check Diagonal Right
+        int maxRight = Math.min(row, board.length - 1 - col);
+        for (int i = 0; i <= maxRight; i++)
+
+        {
+            if (board[row - i][col + i])
+                return false;
+        }
+        return true;
+    }
 }
