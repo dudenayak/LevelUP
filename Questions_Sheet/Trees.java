b5631998e070f5b2d76972856f749e5eefdc6dd2
dudenayak <deveshjain2k1@gmail.com>
2022-05-13 18:25:12 +0530

    Update Trees.java

--- Questions_Sheet/Trees.java
+++ Questions_Sheet/Trees.java
@@ -54,4 +54,51 @@ public class Trees {
     } else {
         return false;
     }
+
+    // LEETCODE 938
+
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null)
+            return 0;
+
+        if (root.val >= low && root.val <= high) {
+            return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+        } else {
+            return rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+        }
+    }
+
+    // LEETCODE 101
+
+    public boolean isSymmetric(TreeNode root) {
+        return root == null || isSymmetricHelp(root.left, root.right);
+    }
+
+    public boolean isSymmetricHelp(TreeNode left, TreeNode right) {
+        if (left == null || right == null) {
+            return left == right;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+
+        return isSymmetricHelp(left.left, right.right) && isSymmetricHelp(left.right, right.right);
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return Helper(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode Helper(int[] nums, int start, int end) {
+        if (start > end)
+            return null;
+
+        int mid = (start + end) / 2;
+
+        TreeNode root = new TreeNode(nums[mid]);
+
+        root.left = Helper(nums, start, mid - 1);
+        root.right = Helper(nums, mid + 1, end);
+        return root;
+    }
 }
