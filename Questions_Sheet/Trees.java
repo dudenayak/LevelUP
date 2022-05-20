ebe53c3fee9e7f5692e15eaece80ad390952113a
dudenayak <deveshjain2k1@gmail.com>
2022-05-20 22:03:50 +0530

    Update Trees.java

--- Questions_Sheet/Trees.java
+++ Questions_Sheet/Trees.java
@@ -496,4 +496,31 @@ public class Trees {
         }
         return new int[] { -1, -1 };
     }
+
+    // LEETCODE 102. Binary Tree Level Order Traversal
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null)
+            return res;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> currentLevel = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.remove();
+                currentLevel.add(current.val);
+                if (current.left != null) {
+                    queue.add(current.left);
+                }
+                if (current.right != null) {
+                    queue.add(current.right);
+                }
+            }
+            res.add(currentLevel);
+        }
+        return res;
+    }
 }
