3fef627e32e90f8bae73d36613e91d50a81ca559
dudenayak <deveshjain2k1@gmail.com>
2022-06-14 23:30:51 +0530

    Update Graphs.java

--- Questions_Sheet/Graphs.java
+++ Questions_Sheet/Graphs.java
@@ -1,5 +1,8 @@
 package Questions_Sheet;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Graphs {
 
     // GFG BFS of graph
@@ -75,15 +78,118 @@ public class Graphs {
         fill(image, i, j - 1, color, newColor);
     }
 
-    // GFG
+    // GFG Rat in a Maze Problem - I
+
+    class Solution {
+
+        public static int floodFill(int sr, int sc, int[][] vis, int[][] dir, String[] dirS, ArrayList<String> ans,
+                String psf) {
+
+            int n = vis.length, m = vis[0].length;
+
+            if (sr == n - 1 && sc == m - 1) {
+                ans.add(psf);
+                return 1;
+            }
+
+            int count = 0;
+            vis[sr][sc] = 0; // block
+
+            for (int d = 0; d < dir.length; d++) {
+                int r = sr + dir[d][0];
+                int c = sc + dir[d][1];
+                if (r >= 0 && c >= 0 && r < n && c < m && vis[r][c] == 1) {
+                    count += floodFill(r, c, vis, dir, dirS, ans, psf + dirS[d]);
+
+                }
+            }
+            vis[sr][sc] = 1;// unblock
+            return count;
+        }
+
+        public static ArrayList<String> findPath(int[][] m, int n) {
+            // boolean[][] vis = new boolean[n][n];
+            int[][] dir = { { 1, 0 }, { 0, -1 }, { 0, 1 }, { -1, 0 } };
+            String[] dirS = { "D", "L", "R", "U" };
+
+            ArrayList<String> ans = new ArrayList<String>();
+            if (m[0][0] == 0 || m[n - 1][n - 1] == 0)
+                return ans;
+
+            int count = floodFill(0, 0, m, dir, dirS, ans, "");
+            return ans;
+        }
+    }
+
+    // GFG Detect cycle in an undirected graph
+
+    Boolean isCyclicUtil(int v, Boolean visited[], int parent,
+            ArrayList<ArrayList<Integer>> adj) {
+
+        visited[v] = true;
+        Integer i;
+
+        Iterator<Integer> it = adj.get(v).iterator();
+        while (it.hasNext()) {
+            i = it.next();
+
+            if (!visited[i]) {
+                if (isCyclicUtil(i, visited, v, adj))
+                    return true;
+            }
+
+            else if (i != parent)
+                return true;
+        }
+        return false;
+    }
+
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+
+        Boolean visited[] = new Boolean[V];
+        for (int i = 0; i < V; i++)
+            visited[i] = false;
+
+        for (int u = 0; u < V; u++) {
+            if (!visited[u])
+                if (isCyclicUtil(u, visited, -1, adj))
+                    return true;
+        }
+
+        return false;
+    }
 
     // GFG
 
     // GFG
 
-    // GFG
+    // LEETCODE 394. Decode String
 
-    // LEETCODE
+    private int index = 0;
+
+    public String decodeString(String s) {
+        String result = "";
+        int count = 0;
+        while (index < s.length()) {
+            char current = s.charAt(index);
+            if (Character.isLetter(current)) {
+                result += String.valueOf(current);
+            } else if (Character.isDigit(current)) {
+                count = count * 10 + (current - '0');
+            } else if (current == '[') {
+                index++;
+                String temp = decodeString(s);
+                while (count > 0) {
+                    result += temp;
+                    count--;
+                }
+            } else {
+                break;
+            }
+            index++;
+        }
+        return result;
+    }
 
     // LEETCODE
 
