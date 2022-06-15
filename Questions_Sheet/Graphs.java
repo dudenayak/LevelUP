23ba3e10113a6974f286237c7ef01dff1f43248c
dudenayak <deveshjain2k1@gmail.com>
2022-06-15 22:04:09 +0530

    Update Graphs.java

--- Questions_Sheet/Graphs.java
+++ Questions_Sheet/Graphs.java
@@ -2,6 +2,7 @@ package Questions_Sheet;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.Queue;
 
 public class Graphs {
@@ -238,11 +239,111 @@ public class Graphs {
         return result;
     }
 
-    // LEETCODE
+    // LEETCODE 934. Shortest Bridge
 
-    // LEETCODE
+    Queue<int[]> q = new LinkedList<>();
+    private int[][] dirs = new int[][] { { 0, -1 }, { 0, 1 }, { -1, 0 }, { 1, 0 } };
 
-    // LEETCODE
+    public int shortestBridge(int[][] grid) {
+        boolean flag = false;
+
+        for (int i = 0; i < grid.length && !flag; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    dfs(i, j, grid);
+                    flag = true;
+                    break;
+                }
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int[] cell = q.poll();
+            int i = cell[0], j = cell[1], dist = cell[2];
+            for (int[] dir : dirs) {
+                int x = i + dir[0], y = j + dir[1];
+                if (x >= 0 && x < grid.length && y >= 0 && y < grid[0].length) {
+                    if (grid[x][y] == 1)
+                        return dist;
+                    if (grid[x][y] == 0) {
+                        q.add(new int[] { x, y, dist + 1 });
+                        grid[x][y] = 2;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
+    private void dfs(int i, int j, int[][] grid) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != 1)
+            return;
+        grid[i][j] = 2;
+        q.add(new int[] { i, j, 0 });
+        dfs(i, j - 1, grid);
+        dfs(i, j + 1, grid);
+        dfs(i - 1, j, grid);
+        dfs(i + 1, j, grid);
+
+    }
+
+    // LEETCODE 1319. Number of Operations to Make Network Connected
+
+    public int wires(int node, int parent[]) {
+        if (node == parent[node]) {
+            return node;
+        }
+        return parent[node] = wires(parent[node], parent);
+    }
+
+    public int makeConnected(int n, int[][] connections) {
+        int parent[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        int leftWire = 0;
+        int component = 0;
+        int m = connections.length;
+        for (int i = 0; i < m; i++) {
+            int p1 = wires(connections[i][0], parent);
+            int p2 = wires(connections[i][1], parent);
+            if (p1 != p2) {
+                parent[p1] = p2;
+            } else {
+                leftWire++;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (parent[i] == i) {
+                component++;
+            }
+        }
+        return (component - 1) <= leftWire ? component - 1 : -1;
+    }
+
+    // LEETCODE 802. Find Eventual Safe States
+
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        List<Integer> ans = new ArrayList<>();
+        Boolean[] node = new Boolean[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            if (dfs(i, graph, node)) {
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+
+    private boolean dfs(int i, int[][] graph, Boolean[] node) {
+        if (node[i] != null)
+            return node[i];
+        node[i] = false;
+        for (int next : graph[i]) {
+            if (!dfs(next, graph, node))
+                return false;
+        }
+        return node[i] = true;
+    }
 
     // GFG
 
