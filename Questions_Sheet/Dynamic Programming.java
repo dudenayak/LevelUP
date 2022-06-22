a11f61289fa0667a80f8449945fbd6aba2926c61
dudenayak <deveshjain2k1@gmail.com>
2022-06-22 15:25:40 +0530

    Update Dynamic Programming.java

--- Questions_Sheet/Dynamic Programming.java
+++ Questions_Sheet/Dynamic Programming.java
@@ -136,4 +136,36 @@ public class DynamicProgramming {
         }
         return (int) (total % LIMIT);
     }
+
+    // LEETCODE 62. Unique Path
+
+    public static int mazePath(int SR, int SC, int ER, int EC, int[][] dp, int[][] dir) {
+        for (int sr = ER; sr >= SR; sr--) {
+            for (int sc = EC; sc >= SC; sc--) {
+                if (ER == sr && EC == sc) {
+                    dp[sr][sc] = 1;
+                    continue;
+                }
+
+                int count = 0;
+                for (int[] d : dir) {
+                    int r = sr + d[0], c = sc + d[1];
+                    if (r >= 0 && c >= 0 && r < dp.length && c < dp[0].length) {
+                        count += dp[r][c];// mazePath_memo(r, c, er, ec, dp, dir);
+                    }
+                }
+
+                dp[sr][sc] = count;
+            }
+        }
+
+        return dp[SR][SC];
+    }
+
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[n][m];
+        int[][] dir = { { 1, 0 }, { 0, 1 } };
+        return mazePath(0, 0, n - 1, m - 1, dp, dir);
+
+    }
 }
